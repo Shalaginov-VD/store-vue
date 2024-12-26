eb3a7adf9782837972f2c76b1382bb5b665a9b8f
Shalaginov-VD <145598389+Shalaginov-VD@users.noreply.github.com>
2024-12-26 16:06:06 +0500

    Фильтрация товаров по категориям

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="content">
     <h1>Товары</h1>
-    <div class="product-grid">
-      <div class="loading-title" v-if="loading">Загрузка...</div>
+    <div class="filters">
+      <select v-model="selectedCategory" @change="fetchProducts">
+        <option value="">Все категории</option>
+        <option v-for="category in categories" :key="category" :value="category">{{ category }}</option>
+      </select>
+    </div>
+    <div class="loading-title" v-if="loading">Загрузка...</div>
+    <div class="product-grid">   
       <ProductCard
-        v-for="product in products"
+        v-for="product in filteredProducts"
         :key="product.id"
         :product="product"
       />
@@ -23,13 +29,24 @@ export default {
   data() {
     return {
       loading: true,
-      products: []
+      products: [],
+      selectedCategory: '',
+      categories: []
+    }
+  },
+  computed: {
+    filteredProducts() {
+      return this.selectedCategory
+        ? this.products.filter(product => product.category === this.selectedCategory)
+        : this.products
     }
   },
   methods: {
     async fetchProducts() {
+      this.loading = true
       const response = await fetch(`https://fakestoreapi.com/products`)
       this.products = await response.json()
+      this.categories = [...new Set(this.products.map(product => product.category))]
       this.loading = false
     }
   },
