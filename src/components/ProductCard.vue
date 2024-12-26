dfb6ba7f6380d79d7c6fbdbab536e320b44c582c
Shalaginov-VD <145598389+Shalaginov-VD@users.noreply.github.com>
2024-12-26 13:09:04 +0500

    Отображение товаров

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="product-card">
+        <img class="product-img" :src="product.image" alt="Product image">
+        <h2>{{ product.title }}</h2>
+        <p>Цена: ${{ product.price }}</p>
+        <p>Категория: {{ product.category }}</p>
+        <p>
+            Оценка: {{ product.rating.rate }}
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        product: Object
+    }
+}
+</script>
+
+<style>
+.product-card {
+    border: 1px solid #ccc;
+    width: 25%;
+    padding: 15px;
+    margin: 15px;
+}
+
+.product-img {
+    width: 25%;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,47 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="content">
+    <h1>Товары</h1>
+    <div class="product-grid">
+      <div class="loading-title" v-if="loading">Загрузка...</div>
+      <ProductCard
+        v-for="product in products"
+        :key="product.id"
+        :product="product"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import ProductCard from '../components/ProductCard.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    ProductCard
+  },
+  data() {
+    return {
+      loading: true,
+      products: []
+    }
+  },
+  methods: {
+    async fetchProducts() {
+      const response = await fetch(`https://fakestoreapi.com/products`)
+      this.products = await response.json()
+      this.loading = false
+    }
+  },
+  mounted() {
+    this.fetchProducts()
   }
 }
 </script>
+
+<style>
+.product-grid {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
